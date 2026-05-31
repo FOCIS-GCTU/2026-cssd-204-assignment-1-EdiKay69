a7002b2064a589d232209d1b769ccd7287836cb9
EdiKay69 <2425404076@live.gctu.edu.gh>
2026-05-31 00:10:01 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Kenneth Edinam Dogah
+# STUDENT ID: 2425404076
+# Email: 2425404076@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Kenneth Edinam Dogah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large letters
 
+    print()
+    print("...KED")
+    print()
 
+    print("...KKKK....KKKK........EEEEEEEEEEEE........DDDDDDDD.........")
+    print("...KKKK...KKKK.........EEEEEEEEEEEE........DDDDDDDDDD.......")
+    print("...KKKK..KKKK..........EEEE................DDD.....DDD......")
+    print("...KKKK.KKKK...........EEEE................DDD......DDD.....")
+    print("...KKKKKKKK............EEEEEEEEEEEE........DDD......DDD.....")
+    print("...KKKKKKKK............EEEEEEEEEEEE........DDD......DDD.....")
+    print("...KKKK.KKKK...........EEEE................DDD......DDD.....")
+    print("...KKKK..KKKK..........EEEE................DDD.....DDD......")
+    print("...KKKK...KKKK....**...EEEEEEEEEEEE...**...DDDDDDDDDD.....**")
+    print("...KKKK....KKKK...**...EEEEEEEEEEEE...**...DDDDDDDD.......**")
 
+    print()
 main()
+
